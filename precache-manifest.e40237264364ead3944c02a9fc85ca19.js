ffac7527c9a1744e7c365b79fe3a3a43012ad5a9
Muffakham <muffakham.ali@gmail.com>
2020-07-23 16:26:14 -0700

    Add files via upload

---
+++ precache-manifest.e40237264364ead3944c02a9fc85ca19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb92a50dbf7fb3809817be2357badd4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1d1f06225222e7401ca",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "2e56fca243cccd51d5a0",
+    "url": "/static/js/2.cf06da6c.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.cf06da6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1d1f06225222e7401ca",
+    "url": "/static/js/main.004f53a1.chunk.js"
+  },
+  {
+    "revision": "b4ba650b238755144e2a",
+    "url": "/static/js/runtime-main.e5fc83a6.js"
+  }
+]);
